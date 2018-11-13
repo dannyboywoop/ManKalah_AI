774a370a7d03541cb5a49ccf36984a0185a961a6
Cameron Downer <cameronfdowner@protonmail.com>
2018-11-13 18:08:05 +0000

    Add basic request handler

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
+
 # AI-and-Games-Project-1
+
 Repository for COMP34120:AI and GAMES - Project 1
+
+  
+
+## Manual Requests with Request Handler
+
+Run `RequestHandler.py` first:
+
+  ```python RequestHandler.py```
+
+Run game engine:
+
+```java -jar ManKalah.jar "nc localhost 12346" "java -jar MKRefAgent.jar"```
+
+The following message should appear on your `RequestHandler.py` console:
+```
+Recv: b'START;South' # Raw Message from game engine
+START(position=South) # Parsed object
+```
+You can now type out responses and press enter, for example:
+
+```
+MOVE;1 # My response
+Recv: b'CHANGE;1;7,7,7,7,7,7,7,0,0,8,8,8,8,8,8,1;OPP' # Raw change message
+CHANGE (moveswap=1, state=7,7,7,7,7,7,7,0,0,8,8,8,8,8,8,1, turn=OPP) # parsed object
+```
---
+++ RequestHandler.py
@@ -0,0 +1,88 @@
+import socketserver
+
+
+def as_bytes(string):
+    return string.encode("UTF-8")
+
+
+def as_string(data):
+    return data.decode("UTF-8")
+
+
+class StartMessage(object):
+    def __init__(self, *args):
+        if len(args) != 1:
+            raise Error()
+        self.position = args[0]
+
+    def __str__(self):
+        return "START(position={})".format(self.position)
+
+
+class ChangeMessage(object):
+    def __init__(self, *args):
+        if len(args) != 3:
+            raise Error()
+        self.moveswap = args[0]
+        self.state = args[1]
+        self.turn = args[2]
+
+    def __str__(self):
+        return "CHANGE (moveswap={}, state={}, turn={})".format(self.moveswap, self.state, self.turn)
+
+
+class EndMessage(object):
+    def __str__(self):
+        return "END"
+
+
+class InputParser(object):
+    message_types = {
+        "START": StartMessage,
+        "CHANGE": ChangeMessage,
+        "END": EndMessage
+    }
+
+    def __init__(self, message):
+        message = message.split(";")
+        self.message_type = message[0]
+        self.args = message[1:]
+
+    def get_message(self):
+        message = InputParser.message_types[self.message_type]
+
+        if not message:
+            raise Error()
+
+        return message(*self.args)
+
+
+class RequestHandler(socketserver.BaseRequestHandler):
+    def get_user_input(self):
+        return as_bytes(input() + "\n")
+
+    def parse_message(self):
+        data = as_string(self.data)
+        return InputParser(data).get_message()
+
+    def handle(self):
+        while True:
+            self.data = self.request.recv(1024).strip()
+            if not self.data:
+                break
+            print("Recv: {}".format(self.data))
+
+            message = self.parse_message()
+            print(message)
+
+            self.request.sendall(self.get_user_input())
+
+
+class ReuseAddrTCPServer(socketserver.TCPServer):
+    allow_reuse_address = True
+
+
+if __name__ == "__main__":
+    HOST, PORT = "localhost", 12346
+    server = ReuseAddrTCPServer((HOST, PORT), RequestHandler)
+    server.serve_forever()
