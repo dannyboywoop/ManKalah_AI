dea4e898e2ff13bfa9f94569be087da7d7e2cb12
Daniel Holmes <DanielJholmes@hotmail.co.uk>
2018-11-21 13:13:07 +0000

    Improved style to meet PEP8 standard

--- gameState.py
+++ gameState.py
@@ -1,132 +1,175 @@
-# basic gameState class
-# stores the state of the game with methods to determine subsequent states
 import copy
 
-class gameState:
-    players = {
-        "North" : 0,
-        "South" : 1        
-    }
-    
-    # sets up game board and selects South as the first player
-    def __init__(self, holes=7, seeds=7):
-        self.seeds = seeds
-        self.holes = holes
-        self.board = [seeds]*holes + [0] + [seeds]*holes + [0]
-        self.currentPlayer = "South"
-        self.gameOver = False
-        self.firstTurn = True
-        self.scoreSlots = [self.holes, 2 * self.holes + 1]
-        return
-    
-    # return index of opponents score slot
-    def opponentsScoreSlot(self):
-        opponentsPlayerNumber = (self.players[self.currentPlayer] + 1) % 2
-        return self.scoreSlots[opponentsPlayerNumber]
-      
-    # return index of current players score slot
-    def playersScoreSlot(self):
-        return self.scoreSlots[self.players[self.currentPlayer]]
-    
-    # converts a player's slot number [1-7] into a board index
-    def slotIndex(self, slot, player = None):
-        if (player == None):
-            player = self.currentPlayer
-        if (slot<1 or slot>self.holes):
-            raise Exception("Index out of range")
-        return self.players[player]*(self.holes+1) + slot - 1
 
-    # given the name of one player, returns the name of the other
-    def otherPlayer(self, player = None):
-        if (player == None):
-            player = self.currentPlayer
+# constants for 7,7 Kalah
+HOLES = 7
+SEEDS = 7
+
+
+class GameState:
+    """
+    Stores the state of a kalah game, with
+    methods to determine subsequent states
+    """
+    players = {
+        "North": 0,
+        "South": 1
+    }
+
+    def __init__(self):
+        """Sets up game board and selects South as the first player"""
+        self.board = [SEEDS] * HOLES + [0] + [SEEDS] * HOLES + [0]
+        self.current_player = "South"
+        self.game_over = False
+        self.first_turn = True
+        self.score_holes = [HOLES, 2 * HOLES + 1]  # indices of score holes
+        return
+
+    def _opponents_score_hole(self):
+        """return index of opponents score hole"""
+        opponents_player_number = (self.players[self.current_player] + 1) % 2
+        return self.score_holes[opponents_player_number]
+
+    def _players_score_hole(self):
+        """return index of current players score hole"""
+        return self.score_holes[self.players[self.current_player]]
+
+    def _hole_index(self, hole, player=None):
+        """converts a player's hole number [1-7] into a board index"""
+        if player is None:
+            player = self.current_player
+
+        # check in range
+        if (hole < 1 or hole > HOLES):
+            raise Exception("Index out of range")
+
+        return self.players[player] * (HOLES + 1) + hole - 1
+
+    def _other_player(self, player=None):
+        """given the name of one player, returns the name of the other"""
+        if player is None:
+            player = self.current_player
         if player == "North":
             return "South"
         if player == "South":
             return "North"
 
-    # clone server state
-    def cloneServerState(self, board, currentPlayer):
-        if currentPlayer not in self.players.keys():
+    def clone_server_state(self, board, current_player):
+        """clones server state
+
+        Arguments:
+        board -- list of ints representing the number of seeds in each hole
+        current_player -- the name of the player who's turn it is
+        """
+        # check current_player name is a valid option
+        if current_player not in self.players.keys():
             raise Exception("Invalid player")
-        if (len(board)!=2*(self.holes+1)):
+
+        # check the server state has the corrent number of holes
+        if (len(board) != 2 * (HOLES + 1)):
             raise Exception("Invalid board size")
-        
+
+        # copy server state
         self.board = board
-        self.currentPlayer = currentPlayer
-        
-    # returns a list of available moves
-    def movesAvailable(self):
+        self.current_player = current_player
+
+    def moves_available(self):
+        """returns a list of available moves"""
         moves = []
-        for i in range(1, self.holes+1):
-            if (self.board[self.slotIndex(i)]!=0):
+
+        # for each of the current player's holes, check if it contains seeds
+        for i in range(1, HOLES + 1):
+            if (self.board[self._hole_index(i)] > 0):
+                # if so, hole is a valid move
                 moves += [i]
+
+        # return list of valid moves
         return moves
-    
-    # returns a tuple of player scores: (North's score, South's score)
-    def Scores(self):
-        return self.board[self.scoreSlots[0]],self.board[self.scoreSlots[1]]
-    
-    # returns the resultant gameState after a given move
-    def moveResult(self, pos):
-        # get board index of selected slot
-        selectedPos = self.slotIndex(pos)
-        
-        # get number of seeds in selected slot there's atleast 1
-        numOfSeeds = self.board[selectedPos]
-        if (numOfSeeds < 1):
+
+    def scores(self):
+        """returns a tuple of player scores: (North's score, South's score)"""
+        return self.board[self.score_holes[0]], self.board[self.score_holes[1]]
+
+    def move_result(self, pos):
+        """returns the resultant gameState after a given move
+
+        Arguments:
+        pos -- number [1-7] of the hole to select when making a move
+        """
+        # get board index of selected hole
+        selected_pos = self._hole_index(pos)
+
+        # get number of seeds in selected hole, check there's atleast 1
+        num_of_seeds = self.board[selected_pos]
+        if (num_of_seeds < 1):
             raise Exception("Not a legal move!")
-        
+
         # create a new state as a deep copy of the current one
-        newState = copy.deepcopy(self)
-        
+        new_state = copy.deepcopy(self)
+
         # take seeds from selected hole
-        newState.board[selectedPos] = 0
-        
-        # place seeds in subsequent slots (ignoring opponents score slot)
-        while (numOfSeeds>0):
-            selectedPos+=1
-            
-            # loop around board if end is reached
-            if (selectedPos == len(newState.board)):
-                selectedPos=0
-            
-            # if not opponents score slot, deposit a seed
-            if selectedPos != newState.opponentsScoreSlot():
-                newState.board[selectedPos]+=1
-                numOfSeeds-=1
-        
+        new_state.board[selected_pos] = 0
+
+        # place seeds in subsequent holes (ignoring opponents score hole)
+        while (num_of_seeds > 0):
+            # move around the board
+            selected_pos += 1
+
+            # if end is reached go back to the start
+            if (selected_pos == len(new_state.board)):
+                selected_pos = 0
+
+            # if not opponents score hole, deposit a seed
+            if selected_pos != new_state._opponents_score_hole():
+                new_state.board[selected_pos] += 1
+                num_of_seeds -= 1
+
         # check if landed on own hole
-        if (newState.slotIndex(1)<=selectedPos<=newState.slotIndex(newState.holes)):
+        performed_capture = False
+        if (new_state._hole_index(1)
+                <= selected_pos
+                <= new_state._hole_index(HOLES)):
             # if so check if it has only 1 seed and the opposite hole is empty
-            oppositePos = 2 * newState.holes - selectedPos
-            seedsInCurrentPos = newState.board[selectedPos]
-            seedsInOppositePos = newState.board[oppositePos]
-            if (seedsInCurrentPos==1 and seedsInOppositePos>0):
-                newState.board[selectedPos] = 0
-                newState.board[oppositePos] = 0
-                newState.board[newState.playersScoreSlot()] += 1 + seedsInOppositePos
-                
+            opposite_pos = 2 * HOLES - selected_pos
+            seeds_in_current_pos = new_state.board[selected_pos]
+            seeds_in_opposite_pos = new_state.board[opposite_pos]
+            if (seeds_in_current_pos == 1 and seeds_in_opposite_pos > 0):
+                performed_capture = True
+                new_state.board[selected_pos] = 0
+                new_state.board[opposite_pos] = 0
+                new_state.board[new_state._players_score_hole()] += (
+                        1 + seeds_in_opposite_pos
+                )
+
         # check for game over (currentPlayers side is now empty)
-        if (len(newState.movesAvailable()) == 0):
+        if (len(new_state.moves_available()) == 0):
             # read opponents score
-            opponentsFinalScore = newState.board[newState.opponentsScoreSlot()]
-            
+            opponents_final_score = new_state.board[
+                    new_state._opponents_score_hole()]
+
             # current player gets all remaining seeds
-            currentPlayersFinalScore = newState.seeds*newState.holes*2 - opponentsFinalScore
-            
+            current_players_final_score = (SEEDS * HOLES * 2
+                                           - opponents_final_score
+                                           )
+
             # set all board values to 0
-            newState.board = [0]*(2*self.holes+2)
-            
+            new_state.board = [0] * (2 * HOLES + 2)
+
             # set final scores
-            newState.board[newState.opponentsScoreSlot()] = opponentsFinalScore
-            newState.board[newState.playersScoreSlot()] = currentPlayersFinalScore
-            
-            newState.gameOver = True
-                
-        # change current player if not landed on own slot
-        if (selectedPos != newState.playersScoreSlot() or self.firstTurn):
-            newState.firstTurn = False
-            newState.currentPlayer = self.otherPlayer()
-                
-        return newState
+            new_state.board[new_state._opponents_score_hole()]\
+                = opponents_final_score
+            new_state.board[new_state._players_score_hole()]\
+                = current_players_final_score
+
+            # game over
+            new_state.game_over = True
+
+        # change current player if turn is over
+        if (selected_pos != new_state._players_score_hole()
+                or self.first_turn
+                or performed_capture):
+            new_state.first_turn = False
+            new_state.current_player = self._other_player()
+
+        # return resultant state
+        return new_state
