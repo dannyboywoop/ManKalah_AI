6e65a849b92f89c2ef6ca35e4fbb2f2050539299
Cameron Downer <cameronfdowner@protonmail.com>
2018-11-23 12:11:11 +0000

    Add GameState to GameTree

--- GameTree.py
+++ GameTree.py
@@ -1,3 +1,6 @@
+from gameState import GameState
+
+
 class Node:
     def __init__(self, game_state, is_max_node, tree=None):
         self.game_state = game_state
@@ -5,9 +8,9 @@ class Node:
         self.children = {}
         self.tree = tree
 
-    # TODO: we cannot show tree with all the game state
     def __str__(self, level=0, move=""):
-        output = "  " * level + move + str(self.game_state) + "\n"
+        left_padding = " " * 2 * level
+        output = left_padding + move + "\n" + str(self.game_state) + "\n"
         for key, child in self.children.items():
             output += child.__str__(level+1, str(key)+": ")
         return output
@@ -16,12 +19,12 @@ class Node:
         return len(self.children) == 0
 
     def calculate_children(self, depth=1):
-        # TODO: replace with actual calculation from game state
         if not self.children:
-            self.children = {
-                1: Node(10, True, self.tree),
-                2: Node(11, True, self.tree)
-            }
+            available_moves = self.game_state.moves_available()
+            for move in available_moves:
+                new_state = self.game_state.move_result(move)
+                is_max_node = new_state.current_player == "North"
+                self.children[move] = Node(new_state, is_max_node)
 
     def make_root(self):
         self.tree.root = self
@@ -66,18 +69,9 @@ class GameTree:
 
 
 if __name__ == "__main__":
-    tree = GameTree(Node(1, True))
+    tree = GameTree(Node(GameState(), True))
 
     print(tree)
 
     tree.calculate_children()
-    print(tree)
-
-    tree.calculate_children(2)
-    print(tree)
-
-    tree.calculate_children(3)
-    print(tree)
-
-    tree.root.children[1].make_root()
     print(tree)
