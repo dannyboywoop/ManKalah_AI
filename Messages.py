75c527330a0c0cf7b573cf623d8b222b65eece2d
Daniel Holmes <DanielJholmes@hotmail.co.uk>
2018-11-28 20:25:03 +0000

    Added docustrings to Messages.py

--- Messages.py
+++ Messages.py
@@ -1,10 +1,11 @@
+"""Contains the StartMessage, ChangeMessage, EndMessage, Move classes"""
 import sys
 
 # constants for displaying state as 'pretty' board
-row_padding = " " * 4
-box_template = "[{:2}]"
-row_template = row_padding + (box_template * 7)
-score_template = box_template + (" " * 28) + box_template
+ROW_PADDING = " " * 4
+BOX_TEMPLATE = "[{:2}]"
+ROW_TEMPLATE = ROW_PADDING + (BOX_TEMPLATE * 7)
+SCORE_TEMPLATE = BOX_TEMPLATE + (" " * 28) + BOX_TEMPLATE
 
 
 def as_row(seeds):
@@ -13,7 +14,7 @@ def as_row(seeds):
     Arguments:
     seeds -- list of 7 integers representing one players seeds
     """
-    return row_template.format(*seeds)
+    return ROW_TEMPLATE.format(*seeds)
 
 
 def csv_as_int_list(csv):
@@ -31,6 +32,7 @@ def as_bytes(string):
 
 
 class StartMessage:
+    """Parses the initial message from the server specifying our player"""
     players = {
         "North": 0,
         "South": 1
@@ -43,6 +45,7 @@ class StartMessage:
         self.position = args[0]
 
     def update_game_tree(self, game_tree):
+        """Calculates the initial game_tree for our current player"""
         our_player = StartMessage.players[self.position]
         game_tree.calculate_initial_tree(our_player)
 
@@ -51,6 +54,7 @@ class StartMessage:
 
 
 class ChangeMessage:
+    """Parses server messages detailing a change to the game state"""
     def __init__(self, *args):
         if len(args) != 3:
             raise MessageParseException(
@@ -60,14 +64,20 @@ class ChangeMessage:
         self.turn = args[2]
 
     def _parse_index_or_swap(self, arg):
+        """takes a MOVESWAP string and converts it an integer representation
+
+        Arguments:
+        arg -- a MOVESWAP string
+        """
         if arg == "SWAP":
             return -1
         else:
             return int(arg)
 
     def update_game_tree(self, game_tree):
-        move_index = self.moveswap
-        game_tree.make_move(move_index)
+        """change the root of the game tree based on the move that has
+        just been received"""
+        game_tree.make_move(self.moveswap)
 
     def pretty_board(self):
         """Returns `state` as a 'pretty' board"""
@@ -78,7 +88,7 @@ class ChangeMessage:
         # format data into 'pretty' rows
         north_row = as_row(north_seeds)
         south_row = as_row(south_seeds)
-        score_row = score_template.format(north_score, south_score)
+        score_row = SCORE_TEMPLATE.format(north_score, south_score)
 
         return "{}\n{}\n{}".format(north_row, score_row, south_row)
 
@@ -87,7 +97,9 @@ class ChangeMessage:
 
 
 class EndMessage:
+    """Parses end of game message"""
     def update_game_tree(self, game_tree=None):
+        """Prints termination message before exiting the program"""
         print("End message recieved. Terminating.")
         sys.exit(0)
 
@@ -96,15 +108,19 @@ class EndMessage:
 
 
 class Move:
+    """Basic move data type"""
     def __init__(self, hole_index):
         self.hole_index = hole_index
         self.is_swap = hole_index == -1
 
     def message(self):
+        """Defines the conversion from the integer representation of
+        move to a MOVE message"""
         if self.is_swap:
             return as_bytes("SWAP\n")
         return as_bytes("MOVE;{}\n".format(self.hole_index))
 
 
 class MessageParseException(Exception):
+    """Exception thrown upon failing to parse a message"""
     pass
