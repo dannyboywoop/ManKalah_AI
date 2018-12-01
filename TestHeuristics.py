f919f49c45ab557917128ceae5be8fcb55bf1169
Daniel Holmes <DanielJholmes@hotmail.co.uk>
2018-12-01 12:00:48 +0000

    Deleted HeuristicCompDemo.py

--- TestHeuristics.py
+++ TestHeuristics.py
@@ -1,6 +1,9 @@
 """contains a few basic heuristic functions"""
 
 
+from HeuristicComp import SharedNode, HeuristicCompTree
+
+
 def bad_heuristic(game_state, player):
     """arbitrary heuristic, no useful informational content"""
     return game_state.board[2]
@@ -10,3 +13,6 @@ def test_heuristic(game_state, player):
     """returns the difference in score between the two players"""
     opponent_score = game_state.scores()[game_state._other_player(player)]
     return game_state.scores()[player] - opponent_score
+
+if __name__ == "__main__":
+    HeuristicCompTree(bad_heuristic, test_heuristic).runGame()
